7c4909e0412b0cd368eae0ffb7d3a624d6fcc5b0
DimieruRazvan <dimieru.razvan.s4a@student.ucv.ro>
2024-05-31 15:35:53 +0300

    Delete TSP directory

--- TSP/DFS.h
+++
@@ -1,6 +0,0 @@
-#ifndef DFS_H
-#define DFS_H
-
-void DFS(int current, int count, int cost, int start, bool* visited, int& min_cost, int N, int** distances);
-
-#endif // DFS_H
--- TSP/UCS.h
+++
@@ -1,6 +0,0 @@
-#ifndef UCS_H
-#define UCS_H
-
-int uniformCostSearch(int start, int N, int** distances);
-
-#endif // UCS_H
